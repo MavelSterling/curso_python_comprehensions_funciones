539a70ecdceb710e22985f054476523db41b9d00
MavelSterling <mavelynsl@gmail.com>
2023-02-18 01:51:21 -0500

    Create list_tuples_set.py

---
+++ list_tuples_set.py
@@ -0,0 +1,26 @@
+'''
+Lista: es una colección ordenada y mutable de elementos que pueden ser de diferentes tipos de datos. 
+    Se pueden agregar, eliminar y modificar elementos de la lista. 
+    Las listas se definen utilizando corchetes [ ] y los elementos se separan por comas.
+    
+'''
+
+mi_lista = [1, 'Hola', 2.5, True]
+
+
+'''
+tupla: es similar a una lista en que también es una colección ordenada de elementos, pero a diferencia de las listas, 
+    las tuplas son inmutables, lo que significa que no se pueden modificar una vez creadas. 
+    Las tuplas se definen utilizando paréntesis ( ) y los elementos se separan por comas.
+    
+'''
+mi_tupla = (1, 'Hola', 2.5, True)
+
+
+'''
+Conjunto (set): es una colección no ordenada de elementos únicos y no indexados. Los elementos de un conjunto deben ser inmutables. 
+    Los conjuntos se definen utilizando llaves { } o la función set() y los elementos se separan por comas.
+    
+'''
+
+mi_set = {1, 'Hola', 2.5, True}
