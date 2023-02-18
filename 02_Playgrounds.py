4242382e51f5909718f40ecf7ff7fd5d0d8a091e
MavelSterling <mavelynsl@gmail.com>
2023-02-18 00:57:28 -0500

    Update 02_Playgrounds.py

--- 02_Playgrounds.py
+++ 02_Playgrounds.py
@@ -4,7 +4,8 @@ Para resolver este desafío, tu reto es refactorizar el código base utilizando 
 El código base incluye una lista llamada numbers que contiene números pares e impares. 
     El algoritmo actual selecciona los números pares de esta lista y los agrega a una nueva lista llamada even_numbers.
 
-Tu reto es crear la misma lista utilizando la característica de "List Comprehension" de Python para crear la lista de números pares de manera más concisa y eficiente y el resultado debería quedar en la variable even_numbers_v2. 
+Tu reto es crear la misma lista utilizando la característica de "List Comprehension" de Python para crear la lista de números pares de manera más concisa 
+    y eficiente y el resultado debería quedar en la variable even_numbers_v2. 
     Las dos técnicas deberían de dar el mismo resultado.
     
 '''
@@ -21,3 +22,4 @@ print('v1 =>', even_numbers)
 even_numbers_v2 = [number for number in numbers if number % 2 == 0]
 
 print('v2 =>', even_numbers_v2)
+
