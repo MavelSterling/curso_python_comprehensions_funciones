5e4a85a7679c453ee8ed8f14df21fdfae7e384ea
MavelSterling <mavelynsl@gmail.com>
2023-02-18 00:48:32 -0500

    Update 06_dict.py

--- 06_dict.py
+++ 06_dict.py
@@ -1,11 +1,15 @@
 # Dict comprehension con coniciones
 
+
+#Crear un dict comprehension con una lista
 import random
 countries = ['col', 'mex', 'bol', 'pe']
 
 population_v2 = { country: random.randint(1, 100)  for country in countries}
 print(population_v2)
 
+# crear un dict comprehension con una condicion
+# paises con poblacion mayor a 50
 result = { country: population for (country, population) in population_v2.items() if population > 50 }
 print(result)
 
@@ -13,6 +17,11 @@ text = 'Hola, soy Nicolas'
 unique = { c: c.upper() for c in text if c in 'aeiou' }
 print(unique)
 
+
+#dict comprehension de las vocales de una oracion
+text = 'Hola nuevo mundo'
+unique = { c: c.upper() for c in text if c in 'aeiou'}
+print(unique)
 
 # Contar las vocales
 
