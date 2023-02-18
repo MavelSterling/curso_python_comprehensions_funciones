183738a082e6487d0daa0a0d7ce9628ede278461
MavelSterling <mavelynsl@gmail.com>
2023-02-17 23:12:02 -0500

    Update 02_crud_set.py

--- 02_crud_set.py
+++ 02_crud_set.py
@@ -1,6 +1,21 @@
+'''
+
+Funciones de set:
+
+  - add(): Añade un elemento.
+  - update(): Añade cualquier tipo de objeto iterable como: listas, tuplas.
+  - discard(): Elimina un elemento y si ya existe no lanza ningún error.
+  - remove(): Elimina un elemento y si este no existe lanza el error “keyError”.
+  - pop(): Nos devuelve un elemento aleatorio y lo elimina y si el conjunto está vacío lanza el error “key error”.
+  - clear(): Elimina todo el contenido del conjunto.
+  
+  
+'''
+
+
 set_countries = {'col', 'mex', 'bol'}
 
-size = len(set_countries)
+size = len(set_countries) # Tamaño del conjunto
 print(size)
 
 print('col' in set_countries)
