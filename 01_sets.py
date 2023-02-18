81c01c56720827e67a1eec91ea9f19fecf241801
MavelSterling <mavelynsl@gmail.com>
2023-02-17 23:03:34 -0500

    Update 01_sets.py

--- 01_sets.py
+++ 01_sets.py
@@ -32,3 +32,23 @@ set_numbers = set(numbers)
 print(set_numbers)
 unique_numbers = list(set_numbers)
 print(unique_numbers)
+
+
+'''
+Para crear un set en Python se puede hacer con set() y 
+pasando como entrada cualquier tipo iterable, como puede ser una lista. 
+Se puede ver como a pesar de pasar elementos duplicados como dos 8 y en un orden determinado, 
+al imprimir el set no conserva ese orden y los duplicados se han eliminado.
+
+'''
+
+s = set([5, 4, 6, 8, 8, 1])
+print(s)       #{1, 4, 5, 6, 8}
+print(type(s)) #<class 'set'>
+
+# Se puede hacer lo mismo haciendo uso de {} y 
+# sin usar la palabra set() como se muestra a continuación.
+
+s1 = {5, 4, 6, 8, 8, 1}
+print(s1)       #{1, 4, 5, 6, 8}
+print(type(s1)) #<class 'set'>
