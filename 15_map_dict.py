274cedb2bfbb933591f918d6f2e76d723ce651e7
MavelSterling <mavelynsl@gmail.com>
2023-02-20 01:33:14 -0500

    Update 15_map_dict.py

--- 15_map_dict.py
+++ 15_map_dict.py
@@ -14,12 +14,13 @@ items = [
   }
 ]
 
+# Ahora una lista de los precios con map
 prices = list(map(lambda item: item['price'], items))
-print(items)
-print(prices)
+print("Lista de diccionarios: ",items)
+print("Precio de los diccionarios: ",prices)
 
 def add_taxes(item):
-  item['taxes'] = item['price'] * .19
+  item['taxes'] = item['price'] * .19 # agregar el atributo del impuesto 19%
   return item
 
 new_items = list(map(add_taxes, items))
