074377a7d3a96827d02ecfe36f8ee9d311b7ebcc
MavelSterling <mavelynsl@gmail.com>
2023-02-20 00:48:06 -0500

    Update 13_hof.py

--- 13_hof.py
+++ 13_hof.py
@@ -1,3 +1,18 @@
+'''
+Higher order function: una función dentro de otra función
+
+Una función de Orden Superior o en sus siglas HOF se le lama así solo cuando contiene otras funciones como parámetro de entrada o devuelve una función como salida, es decir que en este caso las funciones que operan a otras funciones se les denomina Higher order function.
+También hay que entender que a estas Funciones de Orden Superior HOF se aplican para funciones y métodos que toman como funciones a los parámetros o devuelven una función como un resultado.
+
+Propiedades de HOF
+    Una función es una instancia de tipo objeto.
+    Puede almacenar una función en una variable.
+    Puede pasar una función como un parámetro a otra función.
+    Puede devolver la función desde una función.
+    Se puede almacenar en una estructura de datos como tablas, listas, etc.
+
+'''
+
 def increment(x):
   return x + 1
 
