f90d8d1c1ee74a55b7aa53a907b9c525ba360e26
MavelSterling <mavelynsl@gmail.com>
2023-02-21 00:49:17 -0500

    Create 20_iter.py

---
+++ 20_iter.py
@@ -0,0 +1,18 @@
+'''
+ITERABLES
+
+Un iterable se define como el objeto que contiene un número contable con valores y este al tener un valor puede recorrer uno a uno los elementos que la contienen como una estructura de datos y operar con ellos, 
+pero a la vez se rigen bajo la instrucción que se le es dada, con lo cual son dependientes de la instrucción a recibir.
+
+Los metodos de su uso son dos:  __iter__() y __next__() .
+'''
+
+for i in range(1, 10):
+  print(i)
+
+my_iter = iter(range(1, 4))
+print(my_iter)
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
