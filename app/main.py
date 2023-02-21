c0ac97fbfe8a1e9f70ae9258270038abde4ac000
MavelSterling <mavelynsl@gmail.com>
2023-02-20 23:55:58 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,5 +20,20 @@ def run():
     charts.generate_bar_chart(labels, values)
   '''
 
+'''
+MÓDULOS COMO SCRIPTS: NAME y MAIN
+
+Cuando utilizamos name == ‘main’ estamos dando dualidad a cierta función para que sea ejecutada en dos archivos distintos.
+Para ello debemos tener en cuenta que su uso esta catalogado de dos maneras:
+
+Se puede ejecutar el archivo como un script.
+Importando el codding de un archivo a otro archivo python.
+
+Para Python, es independiente cual de las dos formas estemos utilizando el código, ya que python define una variable especial 
+llamada __name__ la cual contiene un string y cuyo resultado dependerá de la forma en como sea usada.
+'''
+
+
+# Si ejecuta el archivo desde el terminal
 if __name__ == '__main__':
   run()
