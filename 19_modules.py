1da3edd13b741f3a8d7b7321cecbcf4ac0792499
MavelSterling <mavelynsl@gmail.com>
2023-02-20 23:13:21 -0500

    Update 19_modules.py

--- 19_modules.py
+++ 19_modules.py
@@ -1,27 +1,37 @@
 '''
 MODULO
-Un modulo se puede definir que es lo mismo a una biblioteca de código. Es decir es un archivo que contiene 
-un conjunto de funciones que se pueden aplicar.
+    - Un módulo es un fichero, con extensión *.py y, por tanto, accesible al intérprete de Python y ejecutable por el núcleo de Python. Puede contener:
+    - Variables globales, funciones, clases (patrones de objetos) y sentencias ejecutables.
+    - La programación de clases no la estudiaremos en este curso introductorio.
+    - Un módulo, a su vez, puede importar otros módulos.
 '''
 
 
 import sys
-print(sys.path)
+# Ruta en la que se esta ejecutando
+print(sys.path) 
 
+# Expresiones regulares
 import re
 text = 'Mi numero de telefono es 311 123 121, el codigo del pais es 57, mi numero de la suerte 3'
+# Buscar todos los números del text.
 result = re.findall('[0-9]+', text)
-print(result)
+print("Array con los números del texto: ",result)
 
+# Manejo de horas y fechas
 import time
 timestamp = time.time()
-print(timestamp)
+print(timestamp) #hora actual para el computador
 
+# Hora local
 local = time.localtime()
+# Formato para la hora local
 result = time.asctime(local)
-print(result)
+print("Fecha y hora: ",result)
 
+# Utilidad para menjar listas
 import collections
 numbers = [1,1,2,1,2,1,4,5,3,3,21]
+# Diccionario con la frecuencia de los numeros
 counter = collections.Counter(numbers)
 print(counter)
