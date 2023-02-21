1077ba691fe744e9ae3a55eb26fd7dee69cad91a
MavelSterling <mavelynsl@gmail.com>
2023-02-21 00:16:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 '''
+PAQUETES
+
+Los paquetes son las carpetas que contienen varios módulos y cada uno con una función distinta.
+
+Para ello debe tener siempre un archivo de nombre __init__.py (por lo general esta vacio, ya que así es compatible con programas python versiones anteriores a la 3), con esto le estamos indicado a python que esto se trata de un paquete y no de una carpeta.
+
+Para acceder a los módulos de los paquetes podemos utilizar estas opciones:
+    import nombrecarpeta.nombremodulo
+    from nombrecarpeta import nombremodulo
+    from nombrecarpeta.nombremodulo import 
+'''
+
+'''
 from pkg.mod_1 import func_1, func_2
 from pkg.mod_2 import func_3, func_4
 
