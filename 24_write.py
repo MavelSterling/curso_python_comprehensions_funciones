cde8fb0070fa5623e05ee4ca265d67f6721532b8
MavelSterling <mavelynsl@gmail.com>
2023-02-22 22:23:40 -0500

    Update 24_write.py

--- 24_write.py
+++ 24_write.py
@@ -4,8 +4,13 @@ ESCRIBIR TEXTOS EN PYTHON
 Para poder escribir texto en python solo debemos agregar al parámetro open( ) algunas de las siguientes reglas:
 
 Exception	Description
+“r”	Lectura: Abre por defecto un archivo para su lectura, genera error si este no existe.
 “a”	Agregar: se agregar texto al final del texto.
 “w”	Escribir: Sobre escribir cualquier contenido existente en el archivo texto.
+“x”	Crear: Crea un archivo en especifico, devuelve un error si este no es valido.
+“t”	texto: Valor por defecto, devuelve el resultado como tipo texto.
+“b”	Binario: Devuelve el valor de tipo binario (por ejemplo imágenes)
+
 
 Sintaxis.
 
@@ -14,6 +19,20 @@ Su sintaxis es un simple y fácil de usar, solo es necesario especificar el tipo
 f = open('./text.txt') o file = open('./text.txt')
 
 '''
+f = open('./text.txt', "a")
+f.write("nueva linea de texto1\n")
+f.write("nueva linea de texto2\n")
+f.write("nueva linea de texto3\n")
+
+'''
+Ahora y quizas este comando puede ser muy util como muy peligro ya que “w” lo que realiza es eliminar todo el texto y
+lo sobre escribir con lo que le vayamos a ingresar por eso es de cuidado!.
+
+'''
+f = open('./text.txt', "w")
+f.write("Uppps!! hemos sobre escrito el texto con este nuevo\n")
+f.close()
+
 
 with open('./texs.txt', 'w+') as file:
   for line in file:
