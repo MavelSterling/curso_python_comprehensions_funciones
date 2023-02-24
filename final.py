2c849ea6290d9827f2b1d68a6eb859e64938fd8e
MavelSterling <mavelynsl@gmail.com>
2023-02-23 22:38:15 -0500

    Update final.py

--- final.py
+++ final.py
@@ -18,3 +18,17 @@ new = []
 
 names = {'Nicolas', 'Miguel', 'Juan', 'Nicolas'} 
 print(names) 
+
+def sum(a = 1, b = 0):
+  return a + b
+
+sum(b=5)
+
+a = {1,2}
+b = {2,3}
+print("La respuesta es ",a | b)
+
+
+def sum(x, y):
+	return x + y
+
